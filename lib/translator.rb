68779a8f1e08972571463d71a0fec9f81d451805
Michael Kim <m.kim4247@gmail.com>
2018-10-06 16:04:29 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,6 +34,7 @@ def get_japanese_emoticon(file_path, emoticon)
   load_library(file_path)["get_emoticon"].each do |eng_emo, jap_emo|
     if emoticon = eng_emo 
       jap_emoticon = jap_emo
+    end 
   end 
   jap_emoticon
 end
